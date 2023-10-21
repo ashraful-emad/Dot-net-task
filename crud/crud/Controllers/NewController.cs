5f4a87598a935f4af5a7d127f2e0b9012bed17eb
ashraful-emad <mdemad674@gmail.com>
2023-10-21 11:44:59 +0600

    1st commit

---
+++ crud/crud/Controllers/NewController.cs
@@ -0,0 +1,71 @@
+﻿using crud.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crud.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            var db = new productEntities();
+            var data=db.categorizes.ToList();
+            return View(data);
+        }
+
+
+        [HttpGet]
+        public ActionResult create() {
+        return View();
+        
+        }
+
+        [HttpPost]
+        public ActionResult create(categorize c)
+        {
+            var db = new productEntities();
+            db.categorizes.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id) {
+
+            var db = new productEntities();
+            var exdata = db.categorizes.Find(id);
+            db.categorizes.Remove(exdata);
+            db.SaveChanges();
+            return RedirectToAction("Index,New");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new productEntities();
+            var data = (from c in db.categorizes
+                        where c.id == id
+                        select c).SingleOrDefault();  
+            return View(data);  
+        }
+
+        [HttpPost]
+        public ActionResult Edit(categorize c)
+        {
+            var db = new productEntities();
+            var exdata = db.categorizes.Find(c.id);
+            exdata.name = c.name;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
+
+    }
+}
---
+++ crud/crud/Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+﻿using crud.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crud.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Product_index()
+        {
+            var db = new productEntities();
+            var data= db.products.ToList();
+       
+            return View(data);
+        }
+
+
+        [HttpGet]
+        public ActionResult Product_add()
+        {
+            var db = new productEntities();
+            var cate = db.categorizes.ToList();
+            ViewBag.catelist = new SelectList(cate, "id", "name");
+
+            return View();
+            
+        }
+        [HttpPost]
+        public ActionResult Product_add(product p ) { 
+        var db = new productEntities();
+            db.products.Add( p );
+        db.SaveChanges();
+        return RedirectToAction("Product_index");
+        }
+
+        public ActionResult product_delete(int id)
+        {
+
+            var db = new productEntities();
+            var exdata = db.products.Find(id);
+            db.products.Remove(exdata);
+            db.SaveChanges();
+            return RedirectToAction("Product_index");
+        }
+
+
+
+
+
+        [HttpGet]
+        public ActionResult Edit_product(int id)
+        {
+            var db = new productEntities();
+            var data = (from c in db.products
+                        where c.id == id
+                        select c).SingleOrDefault();
+
+            var cate = db.categorizes.ToList();
+            ViewBag.catelist = new SelectList(cate, "id", "name");
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit_product(product c)
+        {
+            var db = new productEntities();
+            var exdata = db.products.Find(c.id);
+            exdata.name = c.name;
+            exdata.price = c.price;
+            exdata.cateid = c.cateid;
+
+
+            db.SaveChanges();
+            return RedirectToAction("Product_index");
+
+        }
+
+
+
+
+
+    }
+}
